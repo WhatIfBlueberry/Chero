4900ee6a4a71255c490519ca1eaf31ccfdbb1e28
Dylan <Dylan-Pascal@web.de>
2022-12-21 10:49:42 +0100

    New RobotSimulation no longer needs certain dotnet version

---
+++ RobotSimulation/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RobotSimulation
+{
+	class Program
+	{
+		public const string DESTINATION_IP_ADDRESS = "127.0.0.1";
+		public const ushort DESTINATION_PORT = 59152;
+
+		static void Main(string[] args)
+		{
+			Robot r = new Robot(DESTINATION_IP_ADDRESS, DESTINATION_PORT);
+			r.Start();
+
+		}
+	}
+}
