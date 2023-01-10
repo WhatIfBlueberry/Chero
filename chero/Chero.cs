c4ddd941b401f713821aeda4efa6c040e3537e39
Dylan <Dylan-Pascal@web.de>
2023-01-10 19:13:16 +0100

    added fromToMovement.
    Setting base still does not work as expected

--- chero/Chero.cs
+++ chero/Chero.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.ConstrainedExecution;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,6 +10,7 @@ namespace chero
 {
     internal class Chero
     {
+        private (int x, int y) currentPos;
         private RobotControl control;
         IDictionary<char, int> fieldnumber = new Dictionary<char, int>();
         private int fieldLengthMM = 30;
@@ -16,27 +18,6 @@ namespace chero
         public Chero(String ip, int port) {
             this.control = new RobotControl("127.0.0.1", 59152);
             initDictionary();
-        }
-
-        private void initDictionary()
-        {
-            fieldnumber.Add('A', 1);
-            fieldnumber.Add('B', 2);
-            fieldnumber.Add('C', 3);
-            fieldnumber.Add('D', 4);
-            fieldnumber.Add('E', 5);
-            fieldnumber.Add('F', 6);
-            fieldnumber.Add('G', 7);
-            fieldnumber.Add('H', 8);
-        }
-
-        private void initBase()
-        {
-            int status = 2;
-            control.MoveAxesToPosition(new RobotAxisPosition(0, -90, 90, 0, -10, 0));
-            control.SetBase(new RobotFrame(330, -260, 350, 0, 0, 0));
-   
-            control.MoveTCPToPosition(new RobotCartPosition(0, 0, 0, 0, 0, 0, status), RobotCartMoveType.PTP, true);
         }
 
         public void start()
@@ -62,12 +43,25 @@ namespace chero
             MoveRobotLin(p);
         }
 
+        public void moveFromTo(Field from, Field to)
+        {
+            (int x, int y) fromCords = parseInput(from.ToString());
+            (int x, int y) toCords = parseInput(to.ToString());
+            Boolean isAtStartingPos = fromCords.Equals(currentPos);
+            if (!isAtStartingPos)
+            {
+                MoveRobotLin(fromCords);
+            }
+            MoveRobotLin(toCords);
+        }
+
         private void MoveRobotLin((int x, int y) p)
         {
             Console.WriteLine("===================================================");
             Console.WriteLine("Robot current position: " + control.Position);
-            RobotPosition pos = control.MoveTCPToPosition(new RobotCartPosition(p.x * fieldLengthMM, p.y * fieldLengthMM, 0, 0, 90, 0), RobotCartMoveType.LIN, true);
+            RobotPosition pos = control.MoveTCPToPosition(new RobotCartPosition(p.x * fieldLengthMM, p.y * fieldLengthMM, 0, 0, 90, 0), RobotCartMoveType.PTP, true);
             Console.WriteLine("Robot moved Sucessfully to Position: " + pos);
+            this.currentPos = p;
         }
 
         private (int x, int y) parseInput(String input)
@@ -78,6 +72,27 @@ namespace chero
             int y = secondChar - '0';
             return (fieldnumber[firstChar], y);
 
+        }
+
+        private void initDictionary()
+        {
+            fieldnumber.Add('A', 1);
+            fieldnumber.Add('B', 2);
+            fieldnumber.Add('C', 3);
+            fieldnumber.Add('D', 4);
+            fieldnumber.Add('E', 5);
+            fieldnumber.Add('F', 6);
+            fieldnumber.Add('G', 7);
+            fieldnumber.Add('H', 8);
+        }
+
+        private void initBase()
+        {
+            int status = 2;
+            control.MoveAxesToPosition(new RobotAxisPosition(0, -90, 90, 0, -10, 0));
+            control.SetBase(new RobotFrame(330, -260, 350, 0, 0, 0));
+            control.MoveTCPToPosition(new RobotCartPosition(0, 0, 0, 0, 0, 0, status), RobotCartMoveType.PTP, true);
+            this.currentPos = (1, 1);
         }
 
     }
--- chero/Program.cs
+++ chero/Program.cs
@@ -8,7 +8,9 @@ namespace chero
         {
             Chero chero = new Chero("127.0.0.1", 59152);
             chero.start();
-            chero.testrun();
+            //chero.testrun();
+            Console.WriteLine(Field.A1.ToString());
+            chero.moveFromTo(Field.A3, Field.B3);
             chero.stop();
 
         }
