8e1735cbb8ea9d5fc1f646e4de9471b0017e085e
Nicolas <nico.lucius@gmx.de>
2023-03-19 14:42:11 +0100

    worked on Rook

--- chero/Pieces/Rook.cs
+++ chero/Pieces/Rook.cs
@@ -14,13 +14,32 @@ namespace chero.Pieces
 
         public override bool canReach(Field field, bool takes)
         {
-            (int x, int y) current = Helper.parseInput(this.field);
+            (int x, int y) current = Helper.parseInput(this.field); // A3 ^= 13
             (int x, int y) target = Helper.parseInput(field);
             if (current.x != target.x && current.y != target.y)
             {
                 return false;
             }
-            return true;
+            else if (current.x == target.x) // target and current position are on the same column
+            {
+                // for from current y to target y if feld besetzt return false
+                int diff = target.y - current.y;
+                for (int i = 0; i < diff; i++)
+                {
+                    // field = current.x , current.y + i
+                    // if isoccupied(field):
+                    // return false
+                }
+            }
+            else if (current.y == target.y) // target and current position are on the same row
+            {
+                // for from current x to target x if feld besetzt return false
+            }
+            else
+            {
+                return true;
+            }
+            
             // TODO think about what happens if both rooks are on the same line.
         }
 
